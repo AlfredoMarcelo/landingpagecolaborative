fbbd7c85f8f072d8cb48b073955c60458658068a
AlfredoMarcelo <alfredo.supanta.26@gmail.com>
2021-11-03 21:16:40 +0000

    cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 import { Jumbotron } from "./Jumbotron";
-
+import { Footer } from "./Footer";
 //create your first component
 const Home = () => {
 	return (
 		<>
-			<Navbar />
-			<Jumbotron />
+			<header>
+				<Navbar />
+				<Jumbotron />
+			</header>
+			<main></main>
+
+			
 		</>
 	);
 };
