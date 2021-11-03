7077ec91bb7090f1d2872038cbe7395d5466115b
arianrdcas <arian4geeka@gmail.com>
2021-11-03 20:38:14 +0000

    Jumbotron_Card

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import { Navbar } from "./Navbar";
 import { Jumbotron } from "./Jumbotron";
+import { Card } from "./Card";
 
 //create your first component
 const Home = () => {
 	return (
 		<>
 			<Navbar />
-			<Jumbotron />
+			<div className="container">
+				<Jumbotron />
+				<div className="row">
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+					<div className="col-3">
+						<Card />
+					</div>
+				</div>
+			</div>
 		</>
 	);
 };
